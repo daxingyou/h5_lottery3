ca33a1093be47f49b36a3ea87231fd89f6881453
Sam <qianduan123@outlook>
2017-12-14 15:37:36 +0800

    优化

--- src/Mixin.js
+++ src/Mixin.js
@@ -9,14 +9,17 @@ var MyMixin = {
     data:function(){
         return {
             action:{
+                // picurl: 'https://img.will888.cn/photo/pic/',  // 图片地址
+                picurl: 'http://admin.baochiapi.com/photo/pic/',
+
                 forseti: 'http://121.58.234.210:19093/forseti/',  // 测试环境
                 uaa:  'http://121.58.234.210:19093/uaa/',   // 测试环境
                 hermes:  'http://121.58.234.210:19093/hermes/', // 测试环境
+
                 // forseti: 'https://api.88bccp.com/forseti/',   // 线上环境
                 // uaa:'https://api.88bccp.com/uaa/',  // 线上环境
                 // hermes:'https://api.88bccp.com/hermes/',   // 线上环境
-                // picurl: 'https://img.will888.cn/photo/pic/',  // 图片地址
-                picurl: 'http://admin.baochiapi.com/photo/pic/'
+
             },
 
             rootBalance:{
