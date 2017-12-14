2c9a4ac415a4febcdd167935bd863c6c20194b1e
Sam <qianduan123@outlook>
2017-12-14 14:09:26 +0800

    优化

--- src/Mixin.js
+++ src/Mixin.js
@@ -10,12 +10,12 @@ var MyMixin = {
         return {
             action:{
                 forseti: 'http://121.58.234.210:19093/forseti/',  // 测试环境
-                uaa: 'http://121.58.234.210:19093/uaa/',   // 测试环境
-                hermes: 'http://121.58.234.210:19093/hermes/',   // 测试环境
-                // forseti: 'https://api.88bccp.com/forseti/',   // 线上环境
-                // uaa: 'https://api.88bccp.com/uaa/' ,  // 线上环境
-                // hermes: 'https://api.88bccp.com/hermes/',   // 线上环境
-                //picurl: 'https://img.will888.cn/photo/pic/',  // 图片地址
+                uaa:  'http://121.58.234.210:19093/forseti/',   // 测试环境
+                hermes:  'http://121.58.234.210:19093/forseti/', // 测试环境
+                // forseti:  this.getOnlineUrl(),   // 线上环境
+                // uaa: this.getOnlineUrl(),  // 线上环境
+                // hermes: this.getOnlineUrl(),   // 线上环境
+                // picurl: 'https://img.will888.cn/photo/pic/',  // 图片地址
                 picurl: 'http://admin.baochiapi.com/photo/pic/'
             },
 
@@ -91,6 +91,16 @@ var MyMixin = {
     },
 
     methods:{
+        //测试链接
+        // getTestUrl:function () {
+        //     var   API_ROOT='http://121.58.234.210:19093/forseti/';
+        //     return API_ROOT;
+        // },
+        //线上链接
+        // getOnlineUrl:function () {
+        //     var   API_ROOT='https://api.88bccp.com/forseti/';
+        //     return API_ROOT;
+        // },
         // 退出函数
         loginOut:function (type) {
             var _self = this ;
