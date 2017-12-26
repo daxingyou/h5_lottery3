44b02b6a9fdf67f522cb06791a2371f09b1589f8
Sam <qianduan123@outlook>
2017-12-26 19:15:33 +0800

    个人中心

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,9 @@ export default {
         acDetail:{ 
             tabs:[
                 { title:'全部', value:'1', days:[], active:true },
-                { title:'优惠', value:'4', days:[], active:false },
                 { title:'充值', value:'2', days:[], active:false },
                 { title:'提款', value:'3', days:[], active:false },
+                { title:'优惠', value:'4', days:[], active:false },
             ] 
         },
         navScroll:{}, // 左侧选单
