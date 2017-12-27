f43460ef038ffe964b37069ab1a81cdaf8ddd2d4
Dashboard <wenziwenzi1719281685@gmail.com>
2017-12-27 14:51:04 +0800

    router

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -114,12 +114,12 @@
 
                  <!-- <li v-for="lottery in allLottery" v-if="lottery.ifHot==1">-->
                   <li v-for="(lottery,index) in allLottery" v-if="index<10"> <!-- 只展示前面10个 -->
-                    <a class="to_lottery" :href="'/'+gameHref[lottery.cid]" v-if="haslogin">
+                    <router-link class="to_lottery" :href="'/'+gameHref[lottery.cid]" v-if="haslogin">
                       <div :class="'badge'">
                         <img v-lazy="action.picurl+lottery.imgUrl+'/0'">
                            <!--<img v-lazy="'static/frist/images/lotteryicon/cp'+lottery.cid+'.png'">-->
                       </div>
-                    </a>
+                    </router-link>
                       <a class="to_lottery" @click="gotoGame(haslogin)"  v-else>
                           <div :class="'badge'">
                                 <img v-lazy="action.picurl+lottery.imgUrl+'/0'">
@@ -297,15 +297,20 @@ export default {
           if(!this.haslogin){
               this.$refs.autoCloseDialog.open('登录后才可以操作')
               setTimeout(function () {
-                  window.location = '/Login' ;
+                  // window.location = '/Login' ;
+                  _self.$router.push( '/Login' )
+
               },1000)
               return
           }
           if(cla=='CZ'){
-              window.location = '/lobbyTemplate/deposit' ;
+              // window.location = '/lobbyTemplate/deposit' ;
+              _self.$router.push( '/lobbyTemplate/deposit' )
           }
           if(cla=='TK'){
-              window.location = '/lobbyTemplate/Withdrawals' ;
+              // window.location = '/lobbyTemplate/Withdrawals' ;
+              _self.$router.push( '/lobbyTemplate/Withdrawals' )
+
           }
       },
       // 敬请期待
